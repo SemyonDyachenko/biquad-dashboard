3388d28a37e33e74aff9cf1f9ae6f9f8b9183020
семён дьяченко <cemchic05@outlook.com>
2020-08-28 11:49:12 +0300

    backend sceleton

---
+++ dashboard/apps/signup/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django import forms
+
+
+class User(models.Model):
+    user_email = models.CharField('user email',max_length=130)
+    user_name = models.CharField('username',max_length=255)
+    user_password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    user_signup_date = models.DateTimeField('date of signup')
+
+    def __str__(self):
+        return self.user_email
+
+    def get_email(self):
+        return self.user_email
+    
+    def get_name(self):
+        return self.user_name
+
+    def get_signup_date(self):
+        return self.user_signup_date
